4df972f2a64623f1092ebdb4a48b75e375a6d4c1
Francisco Gomes <franciscogomestv@gmail.com>
2019-05-26 12:08:09 +0100

    built the schema

---
+++ app/models/crmModel.ts
@@ -0,0 +1,30 @@
+import * as mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+export const ContactSchema = new Schema({
+  firstName: {
+    type: String,
+    required: "Enter a first name"
+  },
+  lastName: {
+    type: String,
+    required: "Enter a first name"
+  },
+  email: {
+    type: String
+  },
+  phone: {
+    type: Number
+  },
+  course: {
+    type: String
+  },
+  review: {
+    type: String
+  },
+  created_date: {
+    type: Date,
+    default: Date.now
+  }
+});
